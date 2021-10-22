25a771a3290ff6d910365df6f4fbab7b2d064a69
Yuliya Sidarenka <yulka1102@gmail.com>
2021-10-22 19:06:03 +0200

    task 2

--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -1,3 +1,14 @@
 function isEmpty(obj) {
-  // ваш код...
+  for(let prop in obj) {
+    return false;
+  }
+  return true;
 }
+
+let schedule = {};
+
+alert( isEmpty(schedule) );
+
+schedule["8:30"] = "подъём";
+
+alert( isEmpty(schedule) );
