d29c9cb6fc1bc87a26d0764bcaa3794579674ce3
Yuliya Sidarenka <yulka1102@gmail.com>
2021-10-30 22:45:36 +0200

    111

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -1,3 +1,10 @@
 function namify(users) {
-  // ваш код...
+    return users.map(item => item.name);
 }
+
+let vasya = { name: 'Вася', age: 25 };
+let petya = { name: 'Петя', age: 30 };
+let masha = { name: 'Маша', age: 28 };
+
+let users = [ vasya, petya, masha ];
+let names = namify(users); // ['Вася', 'Петя', 'Маша']
