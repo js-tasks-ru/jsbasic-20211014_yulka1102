3f79f5125070daabfb05e547667263ef343b20bf
Yuliya Sidarenka <yulka1102@gmail.com>
2021-11-02 23:25:14 +0100

    task3

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,28 @@
+table = document.querySelector('table');
+
 function highlight(table) {
-  // ваш код...
+ 
+  for(let i = 0; i < table.rows.length; i++) {
+    let row = table.rows[i];
+
+    if(row.cells[3].getAttribute('data-available') == "true") {
+      row.classList.add('available');
+    } else if(row.cells[3].getAttribute('data-available') == "false") {
+      row.classList.add('unavailable');
+    } else if (!(row.cells[3].hasAttribute('data-available'))) {
+      row.setAttribute('hidden', '');
+    };
+
+    if (row.cells[2].innerHTML == 'm') {
+      row.classList.add('male');
+    } else if (row.cells[2].innerHTML == 'f') {
+      row.classList.add('female');
+    };
+
+    if(+(row.cells[1].innerHTML) < 18) {
+      row.style.textDecoration = 'line-through';
+    }
+
+  }
+  return table;
 }
