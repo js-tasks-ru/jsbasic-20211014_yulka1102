429506664cca4f531370cb7dfdae47470c8d2a62
Yuliya Sidarenka <yulka1102@gmail.com>
2021-11-04 10:43:28 +0100

    task 2

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,5 @@
 function toggleText() {
-  // ваш код...
+  let btn = document.querySelector('.toggle-text-button');
+  txt = document.getElementById('text');
+  btn.addEventListener('click', () => txt.hasAttribute('hidden') ? txt.hidden = false : txt.hidden = true);
 }
