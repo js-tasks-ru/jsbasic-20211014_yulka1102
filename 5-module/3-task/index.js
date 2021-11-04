9f874efff4e41c9d5df9d5dd2fac0d40c685b4eb
Yuliya Sidarenka <yulka1102@gmail.com>
2021-11-04 15:58:35 +0100

    carousel

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,35 @@
 function initCarousel() {
-  // ваш код...
+  let btnLeft = document.querySelector('.carousel__arrow_left');
+  let btnRight = document.querySelector('.carousel__arrow_right');
+
+  let container = document.querySelector('.container');
+  let sideBar = document.querySelector('.carousel__inner');
+
+  let slidesCount = sideBar.querySelectorAll('.carousel__slide').length;
+
+  let activeSlideIndex = 0;
+
+  let width = container.offsetWidth;
+  // sideBar.style.left = `-${width * (slidesCount - 1)}px`
+
+  btnLeft.addEventListener('click', () => {
+  if (activeSlideIndex < 0) {
+    btnLeft.style.display = 'none';
+  } else {
+    activeSlideIndex--;
+    btnLeft.style.display = '';
+    sideBar.style.left = `-${activeSlideIndex * width}px`;
+  }
+  });
+
+  btnRight.addEventListener('click', () => {
+    if (activeSlideIndex === (slidesCount - 1)) {
+      btnRight.style.display = 'none';
+    } else {
+      activeSlideIndex++;
+      btnRight.style.display = '';
+      sideBar.style.left = `-${activeSlideIndex * width}px`;
+    }
+  });
+
 }
